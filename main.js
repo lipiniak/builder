3a8381d182014a0eab290e1ba75a99d8cad99a27
lipiniak <lipiniak@gmail.com>
2015-04-09 00:26:34 +0200

    nowa wersja

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
 var Wierzcholek = function (id) {
   this.id = id;
-  this.child;
 };
 
 
 Wierzcholek.prototype.addWierzcholek = function(child) {
-	this.child = child;
+	this.dzieci = new Array();
+	dzieci.push(child);
 }
 
 
 
-//przed push
-
-
-Node.prototype.nodeID = function() {
-  console.log(this.id);
-};
-
-
-var Tree = function(){
-	this.branch = new Array()
-};
-
-Tree.prototype.addBranch(parent, child) {
-	this.branch[parent] = child;
-};
-
-var tree = new Node(0);
-
-tree.nodeID();
-
-function addElement(parent, id) {
-	
-
-}
-function removeElement(id){
-
-}
-function printTree(){
-
-}
